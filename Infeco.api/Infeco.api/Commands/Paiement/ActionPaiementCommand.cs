52d5b158176e321b4dc85cc2b6fc5bc479a11f25
fdepret <fdepret@topinvest.fr>
2023-11-03 12:02:14 +0100

    Mise à jour api

---
+++ Infeco.api/Infeco.api/Commands/Paiement/ActionPaiementCommand.cs
@@ -0,0 +1,14 @@
+﻿using Infeco.Api.Infrastructure.MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infeco.Api.Commands.Locataire
+{
+    public abstract class ActionPaiementCommand : Command
+    {
+        public int LocataireAppartementId { get; set; }
+        public double Montant { get; set; }
+        public int TypePaiementId { get; set; }
+        public DateTime DatePaiement { get; set; }
+        public bool GenererQuittanceLoyer { get; set; } = false;
+    }
+}
